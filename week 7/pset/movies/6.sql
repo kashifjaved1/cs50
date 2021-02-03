e3bb7854db2ba98450de9b1044138fd7020ab6cd
kashifjaved1 <53248414+kashifjaved1@users.noreply.github.com>
2021-02-04 02:06:20 +0500

    finished some pending work
    week 7 and 8 done

---
+++ week 7/pset/movies/6.sql
@@ -0,0 +1,5 @@
+/* SELECT avg(rating) FROM ratings
+JOIN movies ON ratings.movie_id = movies.id
+WHERE year = 2012; */
+
+SELECT avg(rating) FROM ratings where movie_id in (select id from movies where year = 2012);

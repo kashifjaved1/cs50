e0e011206a31df171f69a1e4404a19c8966ebf61
kashifjaved1 <53248414+kashifjaved1@users.noreply.github.com>
2021-02-24 19:46:57 +0500

    BID v1.1

--- BulkImageDownloader/js/background.js
+++ BulkImageDownloader/js/background.js
@@ -1,1 +1,10 @@
-//nothing here yet
+let tid = 0;
+chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
+    tid = tabs[0].id;
+    console.log(tid);
+})
+chrome.runtime.onMessage.addListener((request, sender) => {
+    if(request.action === "reloadContentScript"){
+        chrome.tabs.executeScript(tid, {file: "js/content.js"});
+    }
+})
--- BulkImageDownloader/js/content.js
+++ BulkImageDownloader/js/content.js
@@ -1,12 +1,18 @@
-let imgList = [];
+imgList = [];
+console.log(imgList);
 for(let i = 0; i < document.querySelectorAll("img").length; i++){
     imgList[i] = document.querySelectorAll('img')[i].getAttribute('src');
-    //console.log(imgList[i]);
 }
+
+console.log(imgList.length);
 
 chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
     if(msg.text === "hello"){
-        //sendResponse({list: imgList});
         sendResponse(imgList);
     }
 })
+
+document.addEventListener('scroll', _ => {
+    chrome.runtime.sendMessage({action: "closePopupPage"});
+}, {passive: true});
+
